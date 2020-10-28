e10e36717cdb3ba17075f86a84d2305d36c1e709
Kamlesh Bansod <kamlesh.bansod@gmail.com>
2020-10-28 14:58:39 +0530

    Second commit

---
+++ calc.py
@@ -0,0 +1,6 @@
+self.sevenButton = Button(self, font=("Helvetica", 11), text="7", borderwidth=0, command=lambda: self.appendToDisplay("7"))
+		self.sevenButton.grid(row=1, column=0, sticky="NWNESWSE")
+
+		self.eightButton = Button(self, font=("Helvetica", 11), text="8", borderwidth=0, command=lambda: self.appendToDisplay("8"))
+		self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
+
