a84c13be7b8bb18fca2a0ce3842f8d2e969f1296
Kamlesh Bansod <kamlesh.bansod@gmail.com>
2020-10-28 15:25:14 +0530

    new feature added incalc

--- calc.py
+++ calc.py
@@ -3,4 +3,6 @@ self.sevenButton = Button(self, font=("Helvetica", 11), text="7", borderwidth=0,
 
 		self.eightButton = Button(self, font=("Helvetica", 11), text="8", borderwidth=0, command=lambda: self.appendToDisplay("8"))
 		self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
+Kamlesh
+
 
